1a6649e6a0963880ab156546d0634b6edd0dde50
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-29 22:35:01 +0300

    Create #Configuration.sh

---
+++ #Configuration.sh
@@ -0,0 +1,19 @@
+sudo apt install net-tools apache2-bin rsyslog -y
+sudo apt install apache2 -y
+
+
+
+# Enable CGI Scripts on Apache
+sudo chown -R www-data:www-data /var/www
+sudo chmod go-rwx /var/www
+sudo chmod go+x /var/www
+sudo chgrp -R www-data /var/www
+sudo chmod -R go-rwx /var/www
+sudo chmod -R g+rx /var/www
+sudo chmod -R g+rwx /var/www
+sudo ln -s /etc/apache2/mods-available/cgi.load /etc/apache2/mods-enabled/
+sudo systemctl enable apache2
+sudo systemctl restart apache2
+
+# Set www-data to no password sudo permissions
+echo '%www-data ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
